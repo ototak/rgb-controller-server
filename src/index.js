a9aa83b83d7e0d6723333c2907bb1c1a59dc01ca
tak <ottotak@outlook.com>
2022-10-04 11:10:35 +0900

    Update index.js
    コメント追加　

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ var express = require("express");
 var express_app = express()
 const {Client} = require("openrgb-sdk")
 const {utils} = require("openrgb-sdk")
+const SERVER_PORT=3000;
+const OPENRGB_PORT=6742;
+const OPENRGB_USERNAME="rgbapi";
+const OPENRGB_HOST="localhost";
 async function setRGB(color){
 
 		let violet = utils.hexColor("#"+color)
 
-		const client = new Client("rgbapi",6742,"localhost")
-		await client.connect()
+		const client = new Client(OPENRGB_USERNAME,OPENRGB_PORT,OPENRGB_HOST) 
+		await client.connect() // connect to openrgb.
 
 		const deviceCount = await client.getControllerCount()
 
 		let devices = []
-		for(let i = 0 ; i < deviceCount;i++){
+		for(let i = 0 ; i < deviceCount;i++){// change all devices color.
 				devices.push(await client.getControllerData(i))
 				const colors = Array(devices[i].colors.length).fill({
 
@@ -26,16 +30,13 @@ async function setRGB(color){
 
 		
 
-		await client.disconnect()
+		await client.disconnect()//disconnect
 
 
 }
-var server = express_app.listen(3000 ,function(){
-console.log("Server started!"+server.address().port);});
-
-var sample = {
-		id:222
-}
+var server = express_app.listen(SERVER_PORT,function(){//start server
+console.log("Server started!"+server.address().port);
+});
 
 express_app.get("/api/rgb/:color",function(req,res,next){
 		setRGB(req.params.color)
