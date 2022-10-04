a8a9e55c054f0a1e1632687e050148857fb7e9ac
tak <ottotak@outlook.com>
2022-10-04 11:12:32 +0900

    いらないコード消去、読みやすさ改善

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ var express = require("express");
 var express_app = express()
 const {Client} = require("openrgb-sdk")
 const {utils} = require("openrgb-sdk")
+
 const SERVER_PORT=3000;
+
 const OPENRGB_PORT=6742;
 const OPENRGB_USERNAME="rgbapi";
 const OPENRGB_HOST="localhost";
-async function setRGB(color){
 
+async function setRGB(color){
 		let violet = utils.hexColor("#"+color)
 
 		const client = new Client(OPENRGB_USERNAME,OPENRGB_PORT,OPENRGB_HOST) 
@@ -19,21 +21,17 @@ async function setRGB(color){
 		for(let i = 0 ; i < deviceCount;i++){// change all devices color.
 				devices.push(await client.getControllerData(i))
 				const colors = Array(devices[i].colors.length).fill({
-
 						red:violet.red,
 						green:violet.green,
 						blue:violet.blue
 				})
 				client.updateLeds(i,colors)
 		}
-		
-
-		
-
 		await client.disconnect()//disconnect
 
 
 }
+
 var server = express_app.listen(SERVER_PORT,function(){//start server
 console.log("Server started!"+server.address().port);
 });
